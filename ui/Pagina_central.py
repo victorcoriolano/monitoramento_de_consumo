af1d21da8be1170027b6eb79fee59de1eebebe8a
Gabriel Pires <dasaybarreto724@gmail.com>
2025-05-31 12:51:26 -0300

    adicionando diretório raiz ao path pra encontrar os outros modulos

--- ui/Pagina_central.py
+++ ui/Pagina_central.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Adiciona o diretório raiz do projeto ao path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 
 st.set_page_config(page_title="Monitor de Consumo", layout="wide", page_icon="📈")

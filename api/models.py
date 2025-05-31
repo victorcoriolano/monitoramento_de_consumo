c14c9c98b48f866e3b1de34ea992585d299a8244
Gabriel Pires <dasaybarreto724@gmail.com>
2025-05-31 12:48:09 -0300

    criando arquivo separado pra models

---
+++ api/models.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class ConsumoAgua(BaseModel):
+    usuario_id: int
+    atividade: str
+    volume_litros: float
+    timestamp: datetime
+
+class ConsumoEnergia(BaseModel):
+    usuario_id: int
+    equipamento: str
+    potencia_w: float
+    gasto_h: float
+    timestamp: datetime
+
+
+class Produto(BaseModel):
+    usuario_id: int
+    prod_nome: str
+    unidade: str
+    preco_unitario: float
+    quantidade_restante: float
+    timestamp: datetime
+
+class Compra(BaseModel):
+    usuario_id: int
+    produto_id: int
+    quantidade: float
+    gasto_total: float
+    timestamp: datetime
+
+class Atividade_gasto(BaseModel):
+    usuario_id: int
+    produto_id: int
+    produto_nome: str
+    atividade: str
+    porcentagem_gasto: float
+    consumo: float
+    timestamp: datetime
+
+ 

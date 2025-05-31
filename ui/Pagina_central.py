443cc0918a3754a095b4726f3b384236376250c7
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2025-05-31 07:14:02 -0300

    adicionando page_icon

--- ui/Pagina_central.py
+++ ui/Pagina_central.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="Monitor de Consumo", layout="wide", page_icon="💧")
+st.set_page_config(page_title="Monitor de Consumo", layout="wide", page_icon="📈")
 st.title("📊 Monitor de Consumo Doméstico")
 st.markdown("Use o menu lateral para navegar entre dashboards e inserir dados.")

65aaad08e7cb87fc1c611918b4a416c2c6169afb
Gabriel Pires <dasaybarreto724@gmail.com>
2025-06-01 10:54:58 -0300

    adicionando prints para ver se o arquivo realmente existe

--- ui/db.py
+++ ui/db.py
@@ -5,4 +5,8 @@ from sqlalchemy import create_engine
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # volta para a raiz
 db_path = os.path.join(base_dir, "consumo.db")
 
+# Apenas para debug: veja se o arquivo realmente existe
+print("📁 Caminho do banco:", db_path)
+print("🗂️ Existe?", os.path.exists(db_path))
+
 engine = create_engine(f"sqlite:///../{db_path}")
